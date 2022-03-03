4dfbb9a6ae500952b0b301b74d2a7b85ee936799
Ciaran-06 <76187734+Ciaran-06@users.noreply.github.com>
2022-03-03 19:21:42 +0000

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import React from 'react';
+
+let page = "home";
+let root = document.querySelector(":root");
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <h1>{ page }</h1>
+        <button>Click Me!</button>
+      </div>
     </div>
   );
 }
+
+/*class Option extends React.Component {
+  }
+}*/
 
 export default App;
