f11cf7a4bd1727cccbb139b3a1b90930d0da82e3
Ciaran-06 <76187734+Ciaran-06@users.noreply.github.com>
2022-03-04 14:30:12 +0000

    added basic header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from './logo.png';
 import './App.css';
 import React from 'react';
 import Switch from '@mui/material/Switch';
-
+import { useSwitch } from '@mui/base/SwitchUnstyled';
+ 
+const primary = ["#181A18", "#F5F5F5"];
+const secondary = ["#F5F5F5", "#181A18"];
+//const background = ["black" , "white"];
 let page = "home";
 let root = document.querySelector(":root");
 
@@ -10,10 +14,13 @@ function App() {
   return (
     <div className="App">
       <div className="App-Header">
+        <div className='App-Header-Page'>
         <h1>{ page }</h1>
-        <button onClick={() => changeTheme()}>Click Me!</button>
-        <div className="switch">
-          <span>Light-Mode</span><Switch /><span>Dark</span>
+        </div>
+        <div className="Head-Switch">
+          <h3>Light</h3>
+          <Switch onClick={() => changeTheme()} />
+          <h3>Dark</h3>
         </div>
       </div>
     </div>
@@ -21,12 +28,18 @@ function App() {
 }
 
 function changeTheme() {
-  if (root.style.getPropertyValue("--text") === "black") {
-    root.style.setProperty('--text', 'white');
-    root.style.setProperty('--background', 'black');
+  let theme = -1;
+
+  if (root.style.getPropertyValue("--primary") === "#F5F5F5") {
+    theme = 0;
+    console.log("light");
+    root.style.setProperty('--primary', primary[theme]);
+    root.style.setProperty('--secondary', secondary[theme]);
   } else {
-    root.style.setProperty('--text', 'black');
-    root.style.setProperty('--background', 'white');
+    theme = 1;
+    console.log("dark");
+    root.style.setProperty('--primary', primary[theme]);
+    root.style.setProperty('--secondary', secondary[theme]);
   }
 }
 /*class Option extends React.Component {
